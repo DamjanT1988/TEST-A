2116064cc0a871f7a5932a5b7040660e059adf3a
Angelina <bunny.iblis@gmail.com>
2023-05-09 15:10:51 +0300

    Added Logo component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,11 @@
 import { Layout } from '../Layout/Layout';
 import style from './Header.module.css';
+import { Logo } from './Logo/Logo';
 
 export const Header = () => (
   <header className={style.header}>
     <Layout>
-      
+      <Logo />
     </Layout>
   </header>  
 );
---
+++ src/components/Header/Logo/Logo.jsx
@@ -0,0 +1,8 @@
+import style from './Logo.module.css';
+import logo from './img/logo.png';
+
+export const Logo = () => (
+  <a className={style.link} href='/'>
+    <img className={style.logo} src={logo} alt="Letter F logo" />
+  </a>
+);
