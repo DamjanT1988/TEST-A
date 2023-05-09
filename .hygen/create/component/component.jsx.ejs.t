07da09665bed3876a2faacad9d5afb15492c8d34
Angelina <bunny.iblis@gmail.com>
2023-05-09 13:53:25 +0300

    Added Layout component

--- .hygen/create/component/component.jsx.ejs.t
+++ .hygen/create/component/component.jsx.ejs.t
@@ -1,7 +1,6 @@
 ---
 to: <%= absPath %>/<%= component_name %>.jsx
 ---
-import React from 'react';
 import style from './<%= component_name %>.module.css';
 
 export const <%= component_name %> = () => {
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import { Layout } from '../Layout/Layout';
 import style from './Header.module.css';
 
-export const Header = () => {
-    return <div className={style.container}></div>
-}
+export const Header = () => (
+  <header className={style.header}>
+    <Layout>
+      
+    </Layout>
+  </header>  
+);
