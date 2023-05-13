1a409b40ee8f0221a5f0f8b7d81780e010297126
DamjanT1988 <Damjan_t@hotmail.com>
2023-05-13 20:16:58 +0200

    9

--- src/components/CameraStream/CameraStream.jsx
+++ src/components/CameraStream/CameraStream.jsx
@@ -1,59 +1,44 @@
-import React, { useRef, useEffect, useState } from 'react';
-import { Canvas, useThree, useLoader } from '@react-three/fiber';
-import { OrbitControls, useGLTF } from '@react-three/drei';
+import React, { useRef, useState, useEffect, useCallback } from 'react';
+import style from './CameraStream.module.css';
+import { Canvas, useFrame, useThree } from '@react-three/fiber';
+import { OrbitControls, PerspectiveCamera, PositionalAudio, Html } from '@react-three/drei';
 import Webcam from 'react-webcam';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import { Vector2, Raycaster } from 'three';
+import * as THREE from 'three';
 
 const Model = ({ modelUrl, position }) => {
   const gltf = useLoader(GLTFLoader, modelUrl);
-  return <primitive object={gltf.scene} scale={0.4} position={position} />;
-};
-
-const Scene = () => {
-  const { camera, scene } = useThree();
-  const [modelPosition, setModelPosition] = useState([0, 0, 0]);
-  const modelUrl = '/models/chair/scene.gltf';
-
-  const handleMouseMove = (event) => {
-    const rect = event.target.getBoundingClientRect();
-    const x = event.clientX - rect.left;
-    const y = event.clientY - rect.top;
-
-    const mouse = new Vector2(
-      (x / window.innerWidth) * 2 - 1,
-      -(y / window.innerHeight) * 2 + 1
-    );
-
-    const raycaster = new Raycaster();
-    raycaster.setFromCamera(mouse, camera);
-    const intersects = raycaster.intersectObjects(scene.children, true);
-
-    if (intersects.length > 0) {
-      setModelPosition(intersects[0].point);
-    }
-  };
-
-  useEffect(() => {
-    window.addEventListener('mousemove', handleMouseMove);
-    return () => window.removeEventListener('mousemove', handleMouseMove);
-  }, [handleMouseMove]);
-
-  return <Model modelUrl={modelUrl} position={modelPosition} />;
+  return (
+    <primitive object={gltf.scene} position={position} scale={0.4} />
+  );
 };
 
 const CameraStream = () => {
   const webcamRef = useRef(null);
+  const [modelPosition, setModelPosition] = useState([0, 0, 0]);
+
+  const handleTouchMove = useCallback((event) => {
+    // This is a simplified example. You'll need to map touch coordinates to your 3D space.
+    setModelPosition([event.touches[0].clientX, event.touches[0].clientY, 0]);
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener('touchmove', handleTouchMove);
+    return () => {
+      window.removeEventListener('touchmove', handleTouchMove);
+    };
+  }, [handleTouchMove]);
+
+  const modelUrl = '/models/chair/scene.gltf';
 
   return (
-    <div className="app">
-      <Webcam ref={webcamRef} className="webcamBackground" />
-      <Canvas className="canvasForeground">
+    <div className={style.app}>
+      <Webcam ref={webcamRef} className={style.webcamBackground} />
+      <Canvas className={style.canvasForeground}>
         <ambientLight intensity={0.5} />
         <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
         <pointLight position={[-10, -10, -10]} />
         <OrbitControls />
-        <Scene />
+        <Model modelUrl={modelUrl} position={modelPosition} />
       </Canvas>
       <div>
         <h1>React Fiber + Three.js + Laptop Camera</h1>
